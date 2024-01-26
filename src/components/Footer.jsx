488808f9c33ad861035d4963c4243feaea2488ab
Neal Chris <tmbarnhart1@gmail.com>
2024-01-26 14:42:15 -0500

    Footer for the bottom of all the pages

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+// components/Footer.js
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        {/* Copyright information */}
+        <p>&copy; 2024 Website. All Rights Reserved.</p>
+      </div>
+      <div>
+        {/* Social media links */}
+        <a href="https://www.facebook.com/yourpage">Facebook</a>
+        <a href="https://twitter.com/yourhandle">Twitter</a>
+        <a href="https://www.instagram.com/youraccount">Instagram</a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;

971917c7c706ed3d2eaec533b6b7e64bf3a3a821
Neal Chris <tmbarnhart1@gmail.com>
2024-01-26 14:28:57 -0500

    Add routing configuration using react-router-dom

---
+++ src/index.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, Router, RouterProvider } from 'react-router-dom'
+import App from './App.jsx'
+// Import App components
+import About from './components/About.jsx'
+import Portfolio from './components/PortfolioPage.jsx'
+import Resume from './components/Resume.jsx'
+import Contact from './components/ContactPage.jsx' 
+
+// Create a BrowserRouter instance with route configurations
+const BrowserRouter = createBrowserRouter([
+  {
+    path: '/',
+    // The root element of the application
+    element: <App/>,
+    children: [
+      {
+        // The default route to render when no specific path matches
+        index: true,
+        element: <About/>,
+      },
+      {
+        // Route for the contact page
+        path: 'contact',
+        element: <Contact/>,
+      },
+      {
+        // Route for the resume page
+        path: 'resume',
+        element: <Resume/>,
+      },
+      {
+        // Route for the portfolio page
+        path: 'portfolio',
+        element: <Portfolio/>,
+      },
+    ],
+  },
+]);
+
+// Render the RouterProvider with the BrowserRouter
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={BrowserRouter} />
+);
